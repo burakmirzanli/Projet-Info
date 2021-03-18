fcf210f27d5b81e7c5bd061c655f94e27f0f98ac
LeKoalaSage <80449185+LeKoalaSage@users.noreply.github.com>
2021-03-18 09:39:14 +0100

    Add files via upload

--- Echiquier.java
+++ Echiquier.java
@@ -1,33 +1,14 @@
 
-import javax.swing.*;
-import java.awt.*;
-
-
-public class Echiquier {
-	
-	private final JFrame fenetreJeu;
-	
-	public Echiquier(){
-		this.fenetreJeu = new JFrame ("Jeu d'echecs");
+public class Echiquier extends JPanel{
 		
-		this.fenetreJeu.setLayout(new BorderLayout());
+		// final Case[8][8] tabCase;
 		
-		this.fenetreJeu.setSize(600,600);
-		
-		this.fenetreJeu.add(this.Plateau.BorderLayout.CENTER);
-		
-		this.fenetreJeu.setVisible(true);
-	}
-	
-	public class Plateau extends JPanel {
-		final Case[8][8] tabCase;
-		
-		Plateau(){
+		Echiquier(){
 			super(new GridLayout(8,8));
-			this.tabCase = new Case [][];
+			// this.tabCase = new Case [][];
 			for(int i = 0; i<8; i++){
 				for(int j = 0; j<8; j++){
-					final Case tabcase = new Case();
+					final Case tabcase = new Case(this, i);
 					this.tabCase.add(Case);
 					add(Case);
 				}
@@ -35,22 +16,5 @@ public class Echiquier {
 		 validate();	
 		}
 	}
-	
-	public class Case extends JPanel {
-		
-	}
-	
-	public boolean estOccupe(int l, int c){
-		boolean b = false;
-		if (echequier [l][c] != null){
-			b = true;
-		}
-		return b;
-	}
-	public String getPieceCouleur(int l, int c){
-		if (estOccupe(l, c)){
-			return //couleur de la piece Pierlouis au secours mdr
-		}
-	}
-}
+
 
