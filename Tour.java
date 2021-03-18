77e306ec63d247fb5312f1e1ec073ccc8ebb0fd6
GentiXh <80448947+GentiXh@users.noreply.github.com>
2021-03-18 08:34:48 +0100

    Update Tour.java

--- Tour.java
+++ Tour.java
@@ -19,8 +19,12 @@ public class Tour extends Piece implements interfaceValidite {
 		this.couleur = c;
 	}
 	
-	public boolean deplacementValid(){		 //cette methode pourra bien etre transformee en interface
-		Deplacement val = new Deplacement(); //Deplacement est une classe (voir la diagramme de cahier des charges qu'on a fait)
-		return val.getValidite();			 //getValidite() est la methode de la classe Deplacement qui retourne un boolean vrai en cas d'un deplacement valide d'une piece
+	public boolean deplacementValid(){		
+		boolean valid = false;
+		Position p = new Position(int l, int c);
+		if ((abs(l-p.getLigne())>=0 && abs(c-getColonne())==0) || (abs(l-p.getLigne())==0 && abs(c-getColonne())>=0)){
+			valid = true;
+		}
+		return valid;
 	}
 }
