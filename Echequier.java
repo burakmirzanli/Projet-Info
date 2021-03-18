8bae299d1c180ccc43fa0cc9ce5160431f1199fb
GentiXh <80448947+GentiXh@users.noreply.github.com>
2021-03-18 09:07:33 +0100

    Update Echequier.java

--- Echequier.java
+++ Echequier.java
@@ -9,4 +9,17 @@ public class Echequier{
 	public void debut(){
 		//
 	}
+	
+	public boolean estOccupe(int l, int c){
+		boolean b = false;
+		if (echequier [l][c] != null){
+			b = true;
+		}
+		return b;
+	}
+	public String getPieceCouleur(int l, int c){
+		if (estOccupe(l, c)){
+			return //couleur de la piece Pierlouis au secours mdr
+		}
+	}
 }
