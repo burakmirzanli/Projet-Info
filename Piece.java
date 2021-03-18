cfa9cda82bcf0293a504b1c16d2cccfa039b433c
GentiXh <80448947+GentiXh@users.noreply.github.com>
2021-03-18 09:19:30 +0100

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -20,9 +20,4 @@ public class Piece {
 	public void setCouleur (String c){
 		this.couleur = c;
 	}
-	
-	public boolean deplacementValid(){		 //cette methode pourra bien etre transformee en interface
-		Deplacement val = new Deplacement(); //Deplacement est une classe (voir la diagramme de cahier des charges qu'on a fait)
-		return val.getValidite();			 //getValidite() est la methode de la classe Deplacement qui retourne un boolean vrai en cas d'un deplacement valide d'une piece
-	}
 }
