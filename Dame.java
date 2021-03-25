becda38737107d2cf6691b8ba65bc11308fa1e51
GentiXh <80448947+GentiXh@users.noreply.github.com>
2021-03-25 08:35:07 +0100

    Update Dame.java

--- Dame.java
+++ Dame.java
@@ -1,10 +1,14 @@
 public class Dame extends Piece implements interfaceValidite {
 	
 	private String couleur;
+	private int ligne;
+	private int colonne;
 	
-	public Dame (String couleur){
-		super ("Dame", couleur);
+	public Dame (String couleur, int ligne, int colonne){
+		super ("Dame", couleur, ligne, colonne);
 		this.couleur = couleur;
+		this.ligne = ligne;
+		this.colonne = colonne;
 	}
 	
 	public String toString (){
@@ -17,6 +21,21 @@ public class Dame extends Piece implements interfaceValidite {
 	
 	public void setCouleur (String c){
 		this.couleur = c;
+	}
+	public int getligne(){
+		return this.ligne;
+	}
+	
+	public int getcolonne(){
+		return this.colonne;
+	}
+	
+	public void setligne(int l){
+		this.ligne=l;
+	}
+	
+	public void setColonne(int c){
+		this.colonne=c;
 	}
 	
 	public boolean deplacementValid(int l, int c){		
