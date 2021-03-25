f17aeb532f5df70fef6e44070200a1b0b627ad3e
GentiXh <80448947+GentiXh@users.noreply.github.com>
2021-03-25 09:41:54 +0100

    Update Tour.java

--- Tour.java
+++ Tour.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Tour extends Piece implements interfaceValidite {
 	
 	private String couleur;
@@ -45,7 +46,7 @@ public class Tour extends Piece implements interfaceValidite {
 	public boolean deplacementValid(int l, int c){		
 		boolean valid = false;
 		Position p = new Position(l, c);
-		if ((abs(l-p.getLigne())>=0 && abs(c-getColonne())==0) || (abs(l-p.getLigne())==0 && abs(c-getColonne())>=0)){
+		if ((Math.abs(l-p.getLigne())>=0 && Math.abs(c-getColonne())==0) || (Math.abs(l-p.getLigne())==0 && Math.abs(c-getColonne())>=0)){
 			valid = true;
 		}
 		return valid;
