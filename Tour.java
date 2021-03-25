4031f3b79a8523a5b10fe8494d7aae78bd852adc
GentiXh <80448947+GentiXh@users.noreply.github.com>
2021-03-25 08:40:50 +0100

    Update Tour.java

--- Tour.java
+++ Tour.java
@@ -1,22 +1,42 @@
 public class Tour extends Piece implements interfaceValidite {
 	
 	private String couleur;
+	private int colonne;
+	private int ligne;
 	
-	public Tour (String couleur){
-		super ("Tour", couleur);
+	public Tour (String couleur, int ligne, int colonne){
+		super ("Tour", couleur, ligne, colonne);
 		this.couleur = couleur;
+		this.ligne = ligne;
+		this.colonne = colonne;
 	}
-	
-	public String toString (){
-		return super.toString();
-	}
-	
+
 	public String getCouleur(){
 		return this.couleur;
 	}
 	
 	public void setCouleur (String c){
 		this.couleur = c;
+	}
+	public int getligne(){
+		return this.ligne;
+	}
+	
+	public int getcolonne(){
+		return this.colonne;
+	}
+	
+	public void setligne(int l){
+		this.ligne=l;
+	}
+	
+	public void setColonne(int c){
+		this.colonne=c;
+	}
+	
+	public String toString (){
+		String s = super.toString();
+		return s;
 	}
 	
 	public boolean deplacementValid(int l, int c){		
